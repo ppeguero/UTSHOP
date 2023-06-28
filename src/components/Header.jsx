749116eaa31af694560296ffa3010b3a239c8df8
pipa peguero <frdpgr@gmail.com>
2023-06-28 07:26:31 -0500

    Peguero Tejero Pipa - Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
+import UTSHOPLogo from '/src/assets/UTSHOP_logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
-    <header>
-        <h1 className='text-4xl'>Soy un header</h1>
-    </header>
+      <header className="shadow-lg flex items-center">
+          <nav className='py-4 flex justify-end w-1/2 ml-6'>
+            <ul className='flex items-center'>
+              <li>
+                <a href="/" className=""><img src={UTSHOPLogo} className="w-24"></img></a>
+              </li>
+            </ul>
+          </nav>
+          <nav className='flex justify-end w-1/2'>
+            <ul className='flex space-x-10 mr-14'>
+              <li>
+                  <a href="/carrito"><FontAwesomeIcon icon={faShoppingCart} /></a>
+                </li>
+                <li>
+                  <a href="/contáctanos"><FontAwesomeIcon icon={faPhone} /></a>
+              </li>
+            </ul>
+          </nav>
+      </header>
   )
 }
 
