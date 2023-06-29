8486b269cbdfc88e05275b4cd3e1cf814aca638a
pipa peguero <frdpgr@gmail.com>
2023-06-29 10:23:41 -0500

    Peguero Tejero Pipa - Parte de arriba del carrito

--- src/routes/Carrito.jsx
+++ src/routes/Carrito.jsx
@@ -1,10 +1,28 @@
 import Header from "../components/header"
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Carrito(){
     return (
         <div>
             <Header />
-            <h1>Carrito</h1>
+            <nav className="w-90 h-16 flex items-center flex-shrink-0 border border-gray-400 bg-white shadow-lg">
+                <div className="w-1/2 flex justify-center">
+                    <p className="text-black text-base font-inter font-light">PAGO SEGURO</p>
+                </div>
+                <div className="w-1/2 flex justify-center ">
+                    <NavLink to="/catálogo" className="flex space-x-3">
+                        <div>
+                            <p className="text-black text-base font-inter font-light hover:text-gray-400">SEGUIR COMPRANDO </p>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faPlay} style={{color: "#9ca3af",}}/> 
+                        </div>
+                    </NavLink>
+                </div>
+            </nav>
         </div>
     )
 }
