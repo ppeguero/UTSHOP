61215afc642519a36e0a9f80362d0920c0ad6d37
mezaaziel06 <mezaaziel06@gmail.com>
2023-07-02 11:12:39 -0600

    creamos rama body en git

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -8,5 +8,21 @@ export default function Inicio(){
             <h1>Inicio</h1>
             <Footer />
         </div>
-    )
-}
+        
+
+          <div className="nex-w-6xl w-full m-auto">
+            
+             <div className=" bg-fondo via-white p-10 flex flex-col items-center gap-16">
+            
+             <h1 className= "to-black font-semibold"> Los mejor productos los consigues solo en UT SHOP </h1>
+            
+             <button className="rgb(20,184,166)">Comprar</button>
+            
+             </div>
+            
+            </div>
+            
+            );
+            
+            }
+    
