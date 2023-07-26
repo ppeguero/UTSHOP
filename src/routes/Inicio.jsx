23e7b892680997a92063943c945601ada79cba8d
TheSCodex <eliud1102@outlook.es>
2023-07-26 00:18:51 -0500

    inicio responsive

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -13,8 +13,8 @@ export default function Inicio(){
         <div>
             <Header />
 
-                <nav className="bg-teal-500 border-2 border-solid border-teal-600 sm: p-3">
-                    <ul className="flex text-white text-sm drop-shadow-lg lg:space-x-8 lg:ml-6 lg:p-2 lg:text-lg sm:space-x-4 sm:ml-2 ">
+                <nav className="bg-teal-500 border-2 border-solid border-teal-600 p-3">
+                    <ul className="flex text-white text-sm drop-shadow-lg lg:space-x-8 lg:ml-6 lg:p-2 xl:text-lg space-x-4 ml-2 ">
                         <NavLink to="/">
                             <li className="hover:text-zinc-200">Inicio</li>
                         </NavLink>
@@ -25,16 +25,16 @@ export default function Inicio(){
                 </nav>
 
                 <section className="relative">
-                    <img className="object-cover static contrast-50 sm:object-center hue-rotate-15 lg:h-96  sm: w-[100%] sm:h-40" src="./src/assets/carousel-3.jpg"></img>
-                    
-                    <h1 className="w-80 absolute xl:inset-y-32 xl:right-40 xl:text-5xl xl:text-center font-bold text-white drop-shadow-xl ">Tus <span className="text-teal-500">productos</span>  favoritos...</h1>
-                    <h2 className="absolute inset-y-60 right-56 font-bold text-white drop-shadow-sm text-2xl">en un <span className="text-teal-500">solo</span> lugar.</h2>
-
-                    <div className="absolute xl:inset-y-72 xl:right-64">
-                        <NavLink to="/catalogo">
-                            <button className="rounded-lg  bg-teal-500 text-white p-2 hover:bg-teal-400 drop-shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Ir al catálogo</button>
+                    <img className="object-cover static contrast-50 object-center hue-rotate-15 xl:h-96 w-[100%] h-40" src="./src/assets/carousel-3.jpg"></img>
+                    <div className="xl:ml-0 ml-[15px] xl:mt-0 mt-[-45px]">
+                        <h1 className="w-80 absolute xl:inset-y-32 inset-y-16 xl:right-40 xl:text-5xl text-xl xl:text-center font-bold text-white drop-shadow-xl ">Tus <span className="text-teal-500">productos</span>  favoritos...</h1>
+                        <h2 className="absolute xl:inset-y-60 xl:left-[74%] left-[16px] top-[81px] xl:right-56 font-bold text-white drop-shadow-sm text-xl">en un <span className="text-teal-500">solo</span> lugar.</h2>
+                        <div className="absolute xl:inset-y-72 inset-y-28 xl:right-64">
+                         <NavLink to="/catalogo">
+                             <button className="rounded-lg bg-teal-500 text-white p-2 hover:bg-teal-400 drop-shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Ir al catálogo</button>
                         </NavLink>
-                    </div>
+                        </div>
+                    </div>    
                 </section>
 
                 <section>
@@ -84,12 +84,12 @@ export default function Inicio(){
 
                 <section className="w-[100%] bg-teal-500">
 
-                    <div className="flex items-center">
-                        <div className="p-12">
+                    <div className="flex xl:flex-row flex-col items-center">
+                        <div className="xl:p-12 p-12 pb-2">
                             <h2 className="text-5xl text-white drop-shadow-sm">Tu también sé parte de la familia UT</h2>
                         </div>
 
-                        <div className="mr-48 flex justify-end">
+                        <div className="xl:mr-48 xl:p-0 p-12 flex xl:justify-end">
                             <p className="font-light text-white lg:w-2/5">
                                 Escoge entre una variedad de productos de la Universidad
                                 Tecnológica de Cancún que te ayudarán a estar a la moda, ser
@@ -99,32 +99,32 @@ export default function Inicio(){
                     </div>
 
 
-                    <div className="w-100% flex mx-10 space-x-10 mb-10">
+                    <div className="w-100% flex xl:flex-row flex-col mx-10 xl:space-x-10 xl:mb-10">
 
                         <div className="flex-1">
-                            <img src="./src/assets/ESTUDIANTE-1.jpg" className="w-full" alt="Imagen 1" />
+                            <img src="./src/assets/ESTUDIANTE-1.jpg" className="xl:w-full w-[640px] xl:pb-0 pb-2 rounded-sm" alt="Imagen 1" />
                         </div>
 
-                        <div className="flex-1">
-                            <img src="./src/assets/ESTUDIANTE-2.webp" className="w-full" alt="Imagen 2" />
+                        <div className="flex-1 xl:pb-0 pb-4">
+                            <img src="./src/assets/ESTUDIANTE-2.webp" className="xl:w-full w-[640px] rounded-sm" alt="Imagen 2" />
                         </div>
                     </div>
 
 
                     <div className="bg-white pt-10">
 
-                        <div className="flex justify-center">
+                        <div className="flex xl:flex-row xl:ml-0 ml-4 flex-col justify-center">
                             <h2 className="text-teal-500 text-5xl font-semibold text-center w-1/4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">¿Dónde recojo mis productos?</h2>
                         </div>
 
-                        <div className="flex items-center p-10">
+                        <div className="flex xl:flex-row flex-col items-center p-10">
 
-                                <div className="flex-1 ml-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                                    <iframe className="w-96 h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1264.849903223069!2d-86.84709312504087!3d21.049695191782323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4e81c385c4677f%3A0x539479cfc0929edb!2sUniversidad%20Tecnol%C3%B3gica%20de%20Canc%C3%BAn%2C!5e0!3m2!1ses-419!2smx!4v1689980658216!5m2!1ses-419!2smx" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                <div className="flex-1 xl:ml-28 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                                    <iframe className="xl:w-96 xl:h-96 w-[350px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1264.849903223069!2d-86.84709312504087!3d21.049695191782323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4e81c385c4677f%3A0x539479cfc0929edb!2sUniversidad%20Tecnol%C3%B3gica%20de%20Canc%C3%BAn%2C!5e0!3m2!1ses-419!2smx!4v1689980658216!5m2!1ses-419!2smx" width="600" height="450" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
 
-                                <div className="flex-1 ml-28 space-y-2">
-                                    <div className="w-3/4 ">
+                                <div className="flex-1 xl:ml-28 space-y-2">
+                                    <div className="w-3/4 xl:p-0 p-4 xl:ml-0 ml-8">
                                         <p className="text-teal-500 font-light text-center">
                                             Visita la tienda física en el edificio B de la Universidad
                                             Tecnológica de Cancún para recoger tus productos.
@@ -132,7 +132,7 @@ export default function Inicio(){
                                     </div>
 
                                     <div>
-                                        <img src="./src/assets/EDIFICIO B.jpg" className="w-3/4 object-cover" alt="Edificio B"></img>
+                                        <img src="./src/assets/EDIFICIO B.jpg" className="xl:w-3/4 object-cover" alt="Edificio B"></img>
                                     </div>
                                 </div>
 
